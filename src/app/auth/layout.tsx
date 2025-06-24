4aab5f2a68362d2ef66f6604882929f320ff5947
ruveyda <ruveydakisla34@gmail.com>
2025-06-25 00:43:15 +0300

    update layout.tsx

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export default function AuthLayout({ children,
 }: Readonly<{
     children: React.ReactNode;
@@ -6,18 +8,23 @@ export default function AuthLayout({ children,
         <div className="min-h-screen grid grid-cols-1 lg:grid-cols-8">
             <div className="col-span-1 lg:col-span-5 bg-white flex items-center justify-center p-6">
                 <div className="w-full max-w-md rounded-2xl shadow-lg p-8">
-                    <img src="/logo.png" alt="logo" className="w-20 h-20" />
+                    <Image width={80}
+                        height={80} src="/logo.png" alt="logo" className="w-20 h-20" />
                     {children}
                 </div>
             </div>
 
             {/* Image Section */}
             <div className="hidden lg:flex col-span-3 bg-[#FFEDE1] items-end justify-center p-4">
-                <img
-                    src="/login-bg.png"
-                    alt="Login Background"
-                    className="w-[90%] max-h-[80%] relative right-32 object-contain"
-                />
+                <div className="relative w-[90%] h-[80%]  max-h-[80%] right-32">
+                    <Image
+                        src="/login-bg.png"
+                        alt="Login Background"
+                        fill
+                        className="object-contain"
+                    />
+                </div>
+
             </div>
         </div>
     );
