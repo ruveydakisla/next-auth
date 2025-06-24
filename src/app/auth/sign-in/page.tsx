e6eed39a172d0ca31ce2182a074ea933727d65a7
ruveyda <ruveydakisla34@gmail.com>
2025-06-25 00:00:32 +0300

    feat: create sign-in

---
+++ src/app/auth/sign-in/page.tsx
@@ -0,0 +1,26 @@
+
+import LoginForm from '@/components/login-form';
+
+export default function Page() {
+    return (
+        <>
+            <div className="mt-4">
+                <p className="text-gray-400 text-sm">Welcome Back !!!</p>
+                <div className="text-black text-3xl font-bold">Sign in</div>
+            </div>
+            <LoginForm />
+            <div className="mt-8 text-center">
+                <p className="text-gray-400 text-sm">
+                    Don't have an account?{' '}
+                    <a
+                        href="/auth/sign-up"
+                        className="text-[#FF6B6B] font-semibold"
+                    >
+                        Sign Up
+                    </a>
+                </p>
+            </div>
+
+        </>
+    );
+}
